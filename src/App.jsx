9000527c7bb509fb03e1fcbf372fd7712c8c0eaf
tawheedshimul <tawheedshimul@gmail.com>
2023-11-12 17:28:09 +0600

    udate

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Cookies from 'universal-cookie';
 import Chat from './components/Chat';
 import { getAuth, signOut } from 'firebase/auth'; // Assuming you are using Firebase
 
+import './App.css'; // Import the Tailwind styles
+
 const cookies = new Cookies();
 
 function App() {
-  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [isAuth, setIsAuth] = useState(cookies.get('auth-token'));
   const [room, setRoom] = useState(null);
 
   const roomInputRef = useRef(null);
@@ -16,7 +18,7 @@ function App() {
   const signUserOut = async () => {
     try {
       await signOut(auth);
-      cookies.remove("auth-token");
+      cookies.remove('auth-token');
       setIsAuth(false);
       setRoom(null);
     } catch (error) {
@@ -24,29 +26,43 @@ function App() {
     }
   };
 
-  if (!isAuth) {
-    return (
-      <div>
-        <Auth setIsAuth={setIsAuth} />
-      </div>
-    );
-  }
-
   return (
-    <>
-      {room ? (
-        <Chat room={room}></Chat>
-      ) : (
-        <div>
-          <label>Enter The Room</label>
-          <input ref={roomInputRef} type="text" />
-          <button onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
-        </div>
-      )}
-      <div>
-        <button onClick={signUserOut}>Sign Out</button>
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md">
+        {!isAuth ? (
+          <Auth setIsAuth={setIsAuth} />
+        ) : (
+          <>
+            {room ? (
+              <Chat room={room}></Chat>
+            ) : (
+              <div className="flex flex-col items-center space-y-4">
+                <label className="text-lg font-bold mb-4">Enter The Room</label>
+                <input
+                  className="border border-gray-300 p-2 rounded-md w-64"
+                  ref={roomInputRef}
+                  type="text"
+                />
+                <button
+                  className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+                  onClick={() => setRoom(roomInputRef.current.value)}
+                >
+                  Enter Chat
+                </button>
+              </div>
+            )}
+            <div className="mt-4">
+              <button
+                className="bg-red-500 text-white p-2 rounded-md hover:bg-red-600"
+                onClick={signUserOut}
+              >
+                Sign Out
+              </button>
+            </div>
+          </>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
