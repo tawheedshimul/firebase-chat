c83ce2988a5fed1246e015a719441aaf3b8cc132
tawheedshimul <tawheedshimul@gmail.com>
2023-11-12 19:49:01 +0600

    update

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,64 +1,30 @@
-// import React from 'react';
-// import { auth, provider } from "../firebase-config";
-// import { signInWithPopup } from 'firebase/auth';
-// import Cookies from 'universal-cookie';
-
-// const cookies = new Cookies
-
-// function Auth(props) {
-//     const { setIsAuth } = props;
-//     const signInWithGoogle = async () => {
-//         try {
-//             const result = await signInWithPopup(auth, provider);
-//             cookies.set("auth-token", result.user.refreshToken);
-//             setIsAuth(true);
-//         } catch (err) {
-//             console.error(err);
-//         }
-
-//     };
-
-//     return (
-//         <div>
-//             <button onClick={signInWithGoogle}>
-//                 <span className="text-gray-700">Sign in with Google</span>
-//             </button>
-//         </div>
-//     );
-// }
-
-// export default Auth;
-
 import React from 'react';
-import { auth, provider } from '../firebase-config';
+import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from 'firebase/auth';
 import Cookies from 'universal-cookie';
 
-const cookies = new Cookies();
+const cookies = new Cookies
 
-function Auth({ setIsAuth }) {
-  const signInWithGoogle = async () => {
-    try {
-      const { user } = await signInWithPopup(auth, provider);
-      const idToken = await user.getIdToken();
-      
-      cookies.set('auth-token', idToken);
-      setIsAuth(true);
+function Auth(props) {
+    const { setIsAuth } = props;
+    const signInWithGoogle = async () => {
+        try {
+            const result = await signInWithPopup(auth, provider);
+            cookies.set("auth-token", result.user.refreshToken);
+            setIsAuth(true);
+        } catch (err) {
+            console.error(err);
+        }
 
-      console.log('Authentication successful:', user.displayName);
-    } catch (error) {
-      console.error('Authentication error:', error.message);
-    }
-  };
+    };
 
-  return (
-    <div>
-      <button onClick={signInWithGoogle}>
-        <span className="text-gray-700">Sign in with Google</span>
-      </button>
-    </div>
-  );
+    return (
+        <div>
+            <button onClick={signInWithGoogle}>
+                <span className="text-gray-700">Sign in with Google</span>
+            </button>
+        </div>
+    );
 }
 
 export default Auth;
-
