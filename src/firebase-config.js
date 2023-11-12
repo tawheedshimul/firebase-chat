d577636734bf43e13b1f10596dda658f137bc293
tawheedshimul <tawheedshimul@gmail.com>
2023-11-12 19:47:36 +0600

    updatge

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,22 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBf-eU596f2uhGL1WXaxxRC6r2R_lrYnsc",
-//   authDomain: "fir-chat-78861.firebaseapp.com",
-//   projectId: "fir-chat-78861",
-//   storageBucket: "fir-chat-78861.appspot.com",
-//   messagingSenderId: "269492468774",
-//   appId: "1:269492468774:web:75efd567b411dfd51bfae7"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const provider = new GoogleAuthProvider();
-// export const db = getFirestore(app);
-
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -30,13 +11,10 @@ const firebaseConfig = {
   appId: "1:269492468774:web:75efd567b411dfd51bfae7"
 };
 
-// Check if Firebase is already initialized to avoid errors
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
 
-// Create Auth and Firestore instances
-const authInstance = getAuth(app);
-const providerInstance = new GoogleAuthProvider();
-const dbInstance = getFirestore(app);
-
-export { authInstance as auth, providerInstance as provider, dbInstance as db };
 
